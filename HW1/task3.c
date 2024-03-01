db95bc9394e665903dd141a498a11926a45bc87f
Jpoopsie <jpoopsie9@gmail.com>
2024-03-02 00:09:20 +0300

    First commit

---
+++ HW1/task3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("   *\n");
+	printf("  ***\n");
+	printf(" *****\n");
+	printf("*******\n");
+	printf(" HH HH\n"); 
+	printf(" ZZ ZZ\n");
+	return 0;
+}
