410d62df7311ee88397a4beb5da51e3f938973e2
Dmitriy Abozhin <jpoopsie9@gmail.com>
2024-05-14 17:32:36 +0300

    First commit

--- HW8/task16.c
+++ HW8/task16.c
@@ -4,9 +4,11 @@
 void reverse_string()*/
 
 #include <stdio.h>
+#include <locale.h>
+
 void reverse(char *str)
 {
-
+	setlocale(LC_ALL, "ru_RU.UTF-8");
 	if (*str)
 	{
 		reverse(str + 1);
@@ -15,6 +17,7 @@ void reverse(char *str)
 }
 int main()
 {
+	setlocale(LC_ALL, "ru_RU.UTF-8");
 	char a[1000], ch;
 	int i = 0;
 	while (scanf("%c", &ch) == 1)
