f9dcf71b0306d8cbbb6b1c5b4f12a96023fe0c4a
Dmitriy Abozhin <jpoopsie9@gmail.com>
2024-05-21 13:58:18 +0300

    First commit

--- HW10/task11.c
+++ HW10/task11.c
@@ -20,30 +20,91 @@ int separator(char *ch, int number)
 }
 
 /*Создаём первый массив в который кладём первое слово до пробела и убираем повторяющиеся буквы*/
-void firstArray(char *ch, int number)
+char *firstArray(char *ch, int number)
 {
 	int count = separator(ch, number);
-	char arr1[count];
-	int meter = 0, k = 0;
+	// printf("%d\n", count);
+	static char arr[100];
+	int meter, k = 0;
 	for (int i = 0; i < count; i++)
 	{
-		for (int j = i + 1; j <= count; j++)
+		meter = 0;
+		for (int j = 0; j <= count; j++)
 		{
 			if (ch[i] == ch[j])
 				meter++;
 		}
-		if (meter == 0)
+		if (meter == 1)
+			arr[k++] = ch[i];
+	}
+	return arr;
+}
+
+/*Создаём второй массив в который кладём первое слово до пробела и убираем повторяющиеся буквы*/
+char *secondArray(char *ch, int number)
+{
+	int count = separator(ch, number);
+	static char arr1[100];
+	// printf("%d\n", count);
+	int meter, k = 0;
+	for (int i = count + 1; i < number; i++)
+	{
+		meter = 0;
+		for (int j = count + 1; j <= number; j++)
+		{
+			if (ch[i] == ch[j])
+				meter++;
+		}
+		if (meter == 1)
 			arr1[k++] = ch[i];
 	}
-	for (int i = 0; i < k; i++)
-		printf("%c", arr1[i]);
+	return arr1;
 }
 
 /*Сравниваем 2 части массива, и ищём одинаковые буквы в обоих частях*/
-// void comparison(char *ch, int number)
-// {
-// 	int count = separator(ch, number);
-// }
+void comparison(char *ch, int number)
+{
+	FILE *output;
+	char array[100];
+	int count = 0, tmp;
+	char *first = firstArray(ch, number);
+	char *second = secondArray(ch, number);
+	int firstLen = 0;
+	int secondLen = 0;
+
+	while (first[firstLen] != '\0')
+		firstLen++;
+	while (second[secondLen] != '\0')
+		secondLen++;
+	for (int i = 0; i < firstLen; i++)
+	{
+		for (int j = 0; j < secondLen; j++)
+		{
+			if (first[i] == second[j])
+				array[count++] = first[i];
+		}
+	}
+
+	/* Cортировка массива по лексиграфическому порядку*/
+	for (int i = 0; i < count; i++)
+	{
+		for (int j = i + 1; j < count; j++)
+		{
+			if (array[i] > array[j])
+			{
+				tmp = array[i];
+				array[i] = array[j];
+				array[j] = tmp;
+			}
+		}
+	}
+	output = fopen("output.txt", "w");
+	for (int i = 0; i < count; i++)
+		fprintf(output, "%c ", array[i]);
+	fclose(output);
+	printf("%s %d\n", first, firstLen);
+	printf("%s %d", second, secondLen);
+}
 
 int main(void)
 {
@@ -54,6 +115,6 @@ int main(void)
 	while ((c = fgetc(input)) != EOF && (c != '\n'))
 		ch[i++] = c;
 	fclose(input);
-	firstArray(ch, i);
+	comparison(ch, i);
 	return 0;
 }
