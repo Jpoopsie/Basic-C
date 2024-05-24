93e215f92fd8a74a21870b8f4e3e8abf646e58bf
Dmitriy Abozhin <jpoopsie9@gmail.com>
2024-05-24 12:08:51 +0300

    First commit

--- HW10/task24.c
+++ HW10/task24.c
@@ -7,6 +7,7 @@ is_palindrom(string)*/
 
 #include <stdio.h>
 
+/*Удаление пробелов в строке*/
 char *delete_space(char *string, int size, int *sizeArr)
 {
 	int arrSize = 0;
@@ -20,6 +21,39 @@ char *delete_space(char *string, int size, int *sizeArr)
 	return array;
 }
 
+/*Сортировка массива*/
+char *sort_array(char *string, int size)
+{
+	static char mainArray[1000];
+	char dubleArr[1000], oneArr[1000];
+	int arrSize = 0;
+	char *extArray = delete_space(string, size, &arrSize);
+	for (int i = 0; i < arrSize; i++)
+	{
+		for (int j = i + 1; j < arrSize; j++)
+		{
+			if (extArray[i] > extArray[j])
+			{
+				char tmp = extArray[i];
+				extArray[i] = extArray[j];
+				extArray[j] = tmp;
+			}
+		}
+	}
+	for (int i = 0; i < arrSize; i++)
+		dubleArr[i] = extArray[i];
+	int a = 0, b = arrSize - 1;
+	for (int i = 0; i < arrSize; i++)
+	{
+		if (extArray[i] == extArray[i + 1])
+		{
+			dubleArr[a++] = extArray[i];
+			dubleArr[b--] = extArray[i + 1];
+		}
+	}
+	
+}
+
 int main(void)
 {
 	int size = 0, arrSize;
@@ -30,8 +64,8 @@ int main(void)
 			break;
 		string[size++] = ch;
 	}
-	char *first = delete_space(string, size, &arrSize);
-	for (int i = 0; i < arrSize; i++)
+	char *first = sort_array(string, size);
+	for (int i = 0; i < size; i++)
 		printf("%c", first[i]);
 	return 0;
 }
