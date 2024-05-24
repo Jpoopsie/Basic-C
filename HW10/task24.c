c242d472cf3234909e9c9927422b36741231915c
Dmitriy Abozhin <jpoopsie9@gmail.com>
2024-05-24 11:39:21 +0300

    First commit

--- HW10/task24.c
+++ HW10/task24.c
@@ -1,153 +1,37 @@
 /*
-Составить палиндром
-Разработать функцию дана строка из маленьких английских букв.
-Составить из символов палиндром максимальной длинны.
-При составлении палиндрома буквы в палиндроме должны быть расположены в лексикографическом порядке.
-Записать ответ в файл output.txt.*/
+Проверка на палиндром
+Считать предложение из файла input.txt и определить можно ли из английских букв предложения записанного в файле получить одно слово - палиндром.
+Ответ напечатать на стандартный поток вывода.
+Требуется реализовать логическую функцию и применить ее.
+is_palindrom(string)*/
 
 #include <stdio.h>
 
-
-/*Убираем пробелы в файле*/
-char *delete_space(char *string, int size, int *sizeArray)
+char *delete_space(char *string, int size, int *sizeArr)
 {
+	int arrSize = 0;
 	static char array[1000];
-	int sizeArr = 0;
-	for(int i = 0; i < size; i++)
-	{
-		if(string[i] != ' ')
-			array[sizeArr++] = string[i];
-	}
-	*sizeArray = sizeArr;
-	return array;
-}
-
-
-/*Сортируем массив по возрастанию*/
-char *spliting_array(char *string, int size)
-{
-	static char array[1000];
-	char temp;
-	int sizeArr;
-	char *mainArray = delete_space(string, size, &sizeArr);
-	for (int i = 0; i < sizeArr; i++)
-	{
-		for (int j = i + 1; j < sizeArr; j++)
-		{
-			if (mainArray[i] > mainArray[j])
-			{
-				temp = mainArray[i];
-				mainArray[i] = mainArray[j];
-				mainArray[j] = temp;
-			}
-		}
-	}
-	for (int i = 0; i < sizeArr; i++)
-		array[i] = mainArray[i];
-	return array;
-}
-
-/*Делаем симетричный массив*/
-char *symmetry_array(char *string, int size, int *sizeArray)
-{
-	static char array[1000];
-	int a = 0, b = size - 1;
-	char *mainArray = spliting_array(string, size);
 	for (int i = 0; i < size; i++)
 	{
-		if (mainArray[i] == mainArray[i + 1])
-		{
-			array[a++] = mainArray[i];
-			array[b--] = mainArray[i + 1];
-			i++;
-		}
+		if (string[i] != ' ')
+			array[arrSize++] = string[i];
 	}
-	for (int i = 0; i < size; i++)
-	{
-		if (array[i] == '\0')
-		{
-			for (int j = i; j < size; j++)
-				array[j] = array[j + 1];
-			i--;
-			size--;
-		}
-	}
-	*sizeArray = size;
+	*sizeArr = arrSize;
 	return array;
-}
-
-/*Ищем одну букву*/
-char *search_letter(char *string, int size)
-{
-	static char array[1000];
-	int count = 1, a = 0;
-	char *symmetryArr = spliting_array(string, size);
-	for (int i = 0; i < size; i++)
-	{
-		count = 1;
-		for (int j = i + 1; j < size; j++)
-		{
-			if (string[i] == string[j])
-			{
-				count++;
-				i++;
-			}
-		}
-		if (count % 2 == 1)
-			array[a++] = symmetryArr[i];
-	}
-	for (int i = 0; i < a; i++)
-	{
-		for (int j = 0; j < a; j++)
-		{
-			if (array[i] < array[j])
-			{
-				char temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-			}
-		}
-	}
-	return array;
-}
-
-/*Вставляем букву в массив*/
-char *insert_letter(char *string, int size, int *sizeArray)
-{
-	static char array[1000];
-	int sizeArr = 0, sizeMainArr;
-	char *symmetry = symmetry_array(string, size, &sizeMainArr);
-	char *oneLetter = search_letter(string, size);
-	for (int i = 0; i < sizeMainArr / 2; i++)
-		array[sizeArr++] = symmetry[i];
-	array[sizeArr++] = oneLetter[0];
-	for (int i = sizeMainArr / 2; i < sizeMainArr; i++)
-		array[sizeArr++] = symmetry[i];
-	*sizeArray = sizeArr;
-	return array;
-}
-
-/*Запись в output file*/
-void write_file(char *string, int size)
-{
-	FILE *output;
-	int sizeArr;
-	char *mainArray = insert_letter(string, size, &sizeArr);
-	output = fopen("output.txt", "w");
-	for (int i = 0; i < sizeArr; i++)
-		fprintf(output, "%c", mainArray[i]);
-	fclose(output);
 }
 
 int main(void)
 {
-	FILE *input;
-	int size = 0;
+	int size = 0, arrSize;
 	char ch, string[1000];
-	input = fopen("input.txt", "r");
-	while ((ch = fgetc(input)) != EOF && (ch != '\n'))
+	while (scanf("%c", &ch))
+	{
+		if (ch == '\n')
+			break;
 		string[size++] = ch;
-	fclose(input);
-	write_file(string, size);
+	}
+	char *first = delete_space(string, size, &arrSize);
+	for (int i = 0; i < arrSize; i++)
+		printf("%c", first[i]);
 	return 0;
 }
