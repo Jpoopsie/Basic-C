33cfced062de463424259b5c7a9d7bf8d899553f
Dmitriy Abozhin <jpoopsie9@gmail.com>
2024-05-24 17:29:04 +0300

    First commit

--- HW10/task26.c
+++ HW10/task26.c
@@ -1,28 +1,60 @@
+/*
+Soundex
+Известный алгоритм Soundex (https://ru.wikipedia.org/wiki/Soundex) определяет, похожи ли два английских слова по звучанию.
+На вход он принимает слово и заменяет его на некоторый четырёхсимвольный код.
+Если коды двух слов совпадают, то слова, как правило, звучат похоже.
+Вам требуется реализовать этот алгоритм. Он работает так:
+Первая буква слова сохраняется.
+В остальной части слова:
+буквы, обозначающие, как правило, гласные звуки: a, e, h, i, o, u, w и y — отбрасываются; оставшиеся буквы (согласные) заменяются на цифры от 1 до 6, причём похожим по звучанию буквам соответствуют одинаковые цифры:
+1: b, f, p, v
+2: c, g, j, k, q, s, x, z
+3: d, t
+4: l
+5: m, n
+6: r
+Любая последовательность одинаковых цифр сокращается до одной такой цифры.
+Итоговая строка обрезается до первых четырёх символов.
+Если длина строки меньше требуемой, недостающие символы заменяются знаком 0.
+Примеры:
+аmmonium → ammnm → a5555 → a5 → a500
+implementation → implmnttn → i51455335 → i514535 → i514*/
+
 #include <stdio.h>
 
-void print_star_triangle(int number)
+/*Удаленин гласных букв*/
+char *delete_vowel(char *string, int size, int *sizeArr)
 {
-	for (int i = 1; i <= number; i++)
+	int arrSize = 0;
+	char array[100];
+	char deleteArr[8] = {"aehiouwy"};
+	for (int i = 0; i < size; i++)
 	{
-		for (int j = 1; j <= number - i; j++)
+		for (int j = 0; j < 8; j++)
 		{
-			printf(" ");
+			if (string[i] != deleteArr[j])
+			{
+				for (int k = 0; k < size; k++)
+					string[k] = string[k + 1];
+				size--;
+				j--;
+			}
 		}
-		for (int j = 1; j <= i; j++)
-		{
-			printf("* ");
-		}
-		printf("\n");
 	}
+	for (int i = 0; i < size; i++)
+		array[arrSize++] = string[i];
 }
 
-int main()
+int main(void)
 {
-	int number;
-	printf("Enter the number of rows: ");
-	scanf("%d", &number);
-
-	print_star_triangle(number);
-
+	int size = 0;
+	char ch, string[20];
+	while (scanf("%c", &ch))
+	{
+		if (ch == '\n')
+			break;
+		string[size++] = ch;
+	}
+	char *del = delete_vowel(string, size, 0);
 	return 0;
 }
