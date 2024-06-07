a0a1b7ab701201ed3ec7eeda24660fc48abe9ba2
Dmitriy Abozhin <jpoopsie9@gmail.com>
2024-06-07 09:19:33 +0300

    First commit

--- Console Reg/main.c
+++ Console Reg/main.c
@@ -14,12 +14,14 @@ void about_program(void)
 
 void registrations(void)
 {
-	// FILE *input;
-	int size = 0;
+	FILE *input;
+	int size = 0, count = 0;
 	char ch, string[1000], txt[4] = ".txt";
 	while (scanf("%c", &ch))
 	{
 		if (ch == '\n')
+			count++;
+		if (count == 2)
 			break;
 		string[size++] = ch;
 	}
@@ -27,7 +29,10 @@ void registrations(void)
 		string[size++] = txt[i];
 	for (int i = 0; i < size; i++)
 		printf("%c", string[i]);
-	// input = fopen(string, "w");
+	input = fopen(string, "w");
+	for(int i = 0; i < size - 4; i++)
+		fprintf(input, "%c", string[i]);
+	fclose(input);
 }
 
 void distributor(char letter)
